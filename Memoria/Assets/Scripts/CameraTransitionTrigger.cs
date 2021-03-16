177d7a4edec492ca7142860133887cc3ab25a42c
Jodelahithit <evancrasbeek@xs4all.nl>
2021-03-16 14:18:33 +0100

    Baked lighting; Improved house; Fixed movement calculation.

---
+++ Memoria/Assets/Scripts/CameraTransitionTrigger.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraTransitionTrigger : MonoBehaviour
+{
+    //public CinemachineVirtualCamera camera;
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.tag == "Player")
+    //    {
+    //        if (camera.Priority == 0)
+    //        {
+    //            camera.Priority = 2;
+    //        }
+    //        else
+    //        {
+    //            camera.Priority = 0;
+    //        }
+    //    }
+    //}
+}
