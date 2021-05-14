c38f6f1243fda942494ce8183424ee9d9f65c2dc
Erwin van Crasbeek <evancrasbeek@xs4all.nl>
2021-05-14 22:52:56 +0200

    Added skybox.

--- Memoria/Assets/editor/MeshCombinerWindow.cs
+++ Memoria/Assets/editor/MeshCombinerWindow.cs
@@ -6,19 +6,48 @@ using UnityEditor.SceneManagement;
 using UnityEngine.SceneManagement;
 
 public class MeshCombinerWindow : EditorWindow {
+    private List<Light> lights = new List<Light>();
+
     [MenuItem("WAEM/Physics tooling")]
     public static void ShowWindow() {
-        GetWindow<MeshCombinerWindow>("WAEM tooling");
+        var window = GetWindow<MeshCombinerWindow>("WAEM tooling");
+        window.Initialize();
     }
 
-    private void OnGUI() {
-        if (GUILayout.Button("Combine selection"))
-        {
-            CombineSelection();
+
+
+    private void Initialize() {
+
+    }
+
+    private void OnDestroy() {
+        lights.Clear();
+    }
+
+    private void OnSelectionChange() {
+        lights.Clear();
+        foreach (var obj in Selection.gameObjects) {
+            Light light = obj.GetComponent<Light>();
+            if (light != null) lights.Add(light);
+            Debug.Log("Yes");
         }
     }
 
-    private void CombineSelection()
-    {
+    private float fl = 1000f;
+    private void OnGUI() {
+        if (GUILayout.Button("Combine selection")) {
+            CombineSelection();
+        }
+
+        if (lights.Count > 0)
+        {
+            fl = EditorGUILayout.Slider(fl, 1000f, 40000f);
+            Debug.Log(Mathf.CorrelatedColorTemperatureToRGB(fl));
+            EditorGUILayout.ColorField(Mathf.CorrelatedColorTemperatureToRGB(fl));
+        }
+        else GUILayout.Label("No lights selected");
+    }
+
+    private void CombineSelection() {
     }
 }
