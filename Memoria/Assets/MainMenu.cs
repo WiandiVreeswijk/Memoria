251cbc6f9c612b5cb38be11e6b3146b00fa028fd
DESKTOP-9M2UNS6\Manfred Mosterdfiets <wiavre@gmail.com>
2021-05-16 15:50:34 +0200

    Added main menu camera; blending between main menu and starting cutscene.

--- Memoria/Assets/MainMenu.cs
+++ Memoria/Assets/MainMenu.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.Playables;
+using Cinemachine;
 
 public class MainMenu : MonoBehaviour
 {
+    public PlayableDirector director;
+    public GameObject controlPanel;
+
+    public CinemachineVirtualCamera mainMenuCamera; 
+    private void Awake()
+    {
+        director.played += Director_Played;
+        director.stopped += Director_Stopped;
+    }
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartTimeLine();
+        mainMenuCamera.Priority = 10;
     }
-
     public void QuitGame()
     {
         Debug.Log("Quit");
         Application.Quit();
+    }
+    private void Director_Played(PlayableDirector obj)
+    {
+        controlPanel.SetActive(true);
+    }
+
+    private void Director_Stopped(PlayableDirector obj)
+    {
+        controlPanel.SetActive(true);
+    }
+
+    public void StartTimeLine()
+    {
+        director.Play();
     }
 }
