09ca8b62d132b4b9efe4bb84f0165eb07c0e228d
Erwin van Crasbeek <evancrasbeek@xs4all.nl>
2021-05-18 05:18:47 +0200

    Improved mesh combiner script; Added coming soon sign to stairs; Baked occlusion culling; Optimized house meshes; Moved some scripts.

--- Memoria/Assets/editor/MeshCombinerWindow.cs
+++ Memoria/Assets/editor/MeshCombinerWindow.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -18,14 +19,14 @@ public class MeshCombinerWindow : ScriptableWizard {
 
     private void OnGUI() {
         if (GUILayout.Button("Combine", GUILayout.Height(100))) {
-            CombineSelection(false);
+            CombineSelectionMultiMat(false);
         }
         if (GUILayout.Button("Combine and save as preset")) {
-            CombineSelection(true);
+            CombineSelectionMultiMat(true);
         }
     }
 
-    private void CombineSelection(bool createPrefab) {
+    private void CombineSelectionMultiMat(bool createPrefab) {
         if (Selection.objects.Length > 1) {
             Debug.LogError("Too many objects selected.");
             return;
@@ -36,70 +37,171 @@ public class MeshCombinerWindow : ScriptableWizard {
         }
 
         var parentOfObjectsToCombine = Selection.activeGameObject;
+        Undo.RegisterCompleteObjectUndo(parentOfObjectsToCombine, "combine");
 
         Vector3 originalPosition = parentOfObjectsToCombine.transform.position;
         parentOfObjectsToCombine.transform.position = Vector3.zero;
 
-        MeshFilter[] meshFilters = Selection.activeGameObject.GetComponentsInChildren<MeshFilter>();
-        Dictionary<Material, List<MeshFilter>> materialToMeshFilterList = new Dictionary<Material, List<MeshFilter>>();
-        List<GameObject> combinedObjects = new List<GameObject>();
+        MeshFilter[] filters = Selection.activeGameObject.GetComponentsInChildren<MeshFilter>();
+        MeshRenderer[] renderers = Selection.activeGameObject.GetComponentsInChildren<MeshRenderer>();
 
-        for (int i = 0; i < meshFilters.Length; i++) {
-            var materials = meshFilters[i].GetComponent<MeshRenderer>().sharedMaterials;
-            if (materials == null) continue;
-            if (materials.Length > 1) {
-                //parentOfObjectsToCombine.transform.position = originalPosition;
-                //Debug.LogError("Multiple materials on meshes. Can't combine");
-                //return;
+        Dictionary<MeshFilter, MeshRenderer> filterRendererPairs = new Dictionary<MeshFilter, MeshRenderer>();
+        foreach (MeshFilter filter in filters) {
+            foreach (var renderer in renderers) {
+                if (filter.gameObject == renderer.gameObject) filterRendererPairs.Add(filter, renderer);
+            }
+        }
+
+        List<Material> materials = new List<Material>();
+
+        foreach (MeshRenderer renderer in renderers) {
+            Material[] localMaterials = renderer.sharedMaterials;
+            foreach (Material localMaterial in localMaterials) {
+                if (!materials.Contains(localMaterial)) materials.Add(localMaterial);
+            }
+        }
+
+        List<Mesh> submeshes = new List<Mesh>();
+        foreach (Material material in materials) {
+            List<CombineInstance> combineInstances = new List<CombineInstance>();
+            foreach (MeshFilter filter in filters) {
+                MeshRenderer renderer = filterRendererPairs[filter];
+                if (renderer == null) {
+                    Debug.LogError(filter.name + "has no MeshRenderer");
+                    continue;
+                }
+
+                Material[] localMaterials = renderer.sharedMaterials;
+                for (int materialIndex = 0; materialIndex < localMaterials.Length; materialIndex++) {
+                    if (localMaterials[materialIndex] != material) continue;
+                    CombineInstance ci = new CombineInstance();
+                    ci.mesh = filter.sharedMesh;
+                    ci.subMeshIndex = materialIndex;
+                    ci.transform = filter.transform.localToWorldMatrix;
+                    combineInstances.Add(ci);
+                }
             }
 
-            Material material = materials[0];
-            if (materialToMeshFilterList.ContainsKey(material)) materialToMeshFilterList[material].Add(meshFilters[i]);
-            else materialToMeshFilterList.Add(material, new List<MeshFilter>() { meshFilters[i] });
+            Mesh mesh = new Mesh();
+            mesh.CombineMeshes(combineInstances.ToArray(), true);
+            submeshes.Add(mesh);
         }
 
-        foreach (var entry in materialToMeshFilterList) {
-            List<MeshFilter> meshesWithSameMaterial = entry.Value;
-            string materialName = entry.Key.ToString().Split(' ')[0];
-            CombineInstance[] combine = new CombineInstance[meshesWithSameMaterial.Count];
-            for (int i = 0; i < meshesWithSameMaterial.Count; i++) {
-                combine[i].mesh = meshesWithSameMaterial[i].sharedMesh;
-                combine[i].transform = meshesWithSameMaterial[i].transform.localToWorldMatrix;
-            }
-
-            Mesh combinedMesh = new Mesh();
-            combinedMesh.CombineMeshes(combine, true, true, true);
-            materialName += "_" + combinedMesh.GetInstanceID();
-            Unwrapping.GenerateSecondaryUVSet(combinedMesh);
-            if (createPrefab) AssetDatabase.CreateAsset(combinedMesh, "Assets/ContentPacks/CombinedMeshes/" + materialName + "Combined.asset");
-
-            string goName = (materialToMeshFilterList.Count > 1) ? materialName + "Combined" : parentOfObjectsToCombine.name + "Combined";
-            GameObject combinedObject = new GameObject(goName);
-            var filter = combinedObject.AddComponent<MeshFilter>();
-            filter.sharedMesh = combinedMesh;
-            var renderer = combinedObject.AddComponent<MeshRenderer>();
-            renderer.sharedMaterial = entry.Key;
-            combinedObjects.Add(combinedObject);
-
-
+        List<CombineInstance> finalCombineInstances = new List<CombineInstance>();
+        foreach (Mesh mesh in submeshes) {
+            CombineInstance ci = new CombineInstance();
+            ci.mesh = mesh;
+            ci.subMeshIndex = 0;
+            ci.transform = Matrix4x4.identity;
+            finalCombineInstances.Add(ci);
         }
 
-        GameObject resultGO = null;
-        if (combinedObjects.Count > 1) {
-            resultGO = new GameObject(parentOfObjectsToCombine.name + "Combined");
-            foreach (var combinedObject in combinedObjects) combinedObject.transform.parent = resultGO.transform;
-        } else {
-            resultGO = combinedObjects[0];
+        Mesh finalMesh = new Mesh();
+        try {
+            finalMesh.CombineMeshes(finalCombineInstances.ToArray(), false);
+        } catch (Exception ex) {
+            Debug.LogError(ex.Message);
+            return;
+        } finally {
+            parentOfObjectsToCombine.transform.position = originalPosition;
         }
+        Unwrapping.GenerateSecondaryUVSet(finalMesh);
+        AssetDatabase.CreateAsset(finalMesh, "Assets/ContentPacks/CombinedMeshes/" + parentOfObjectsToCombine.name + "Combined.asset");
+        GameObject combinedObject = new GameObject(parentOfObjectsToCombine.name + "Combined");
+        var combinedObjectFilter = combinedObject.AddComponent<MeshFilter>();
+        combinedObjectFilter.sharedMesh = finalMesh;
+        var combinedObjectRenderer = combinedObject.AddComponent<MeshRenderer>();
+        combinedObjectRenderer.materials = materials.ToArray();
+        Undo.RegisterCreatedObjectUndo(combinedObject, "combined");
 
-        resultGO.isStatic = true;
+        Debug.Log("Final mesh has " + submeshes.Count + " materials");
+        combinedObject.isStatic = true;
         if (parentOfObjectsToCombine.transform.parent != null) {
-            resultGO.transform.parent = parentOfObjectsToCombine.transform.parent;
+            combinedObject.transform.parent = parentOfObjectsToCombine.transform.parent;
+            int index = parentOfObjectsToCombine.transform.GetSiblingIndex();
+            combinedObject.transform.SetSiblingIndex(index + 1);
         }
 
-        if (createPrefab) PrefabUtility.SaveAsPrefabAssetAndConnect(resultGO, "Assets/ContentPacks/CombinedMeshes" + resultGO.name + ".prefab", InteractionMode.UserAction);
         parentOfObjectsToCombine.SetActive(false);
         parentOfObjectsToCombine.transform.position = originalPosition;
-        resultGO.transform.position = originalPosition;
+        combinedObject.transform.position = originalPosition;
     }
+
+    //private void CombineSelection(bool createPrefab) {
+    //    if (Selection.objects.Length > 1) {
+    //        Debug.LogError("Too many objects selected.");
+    //        return;
+    //    }
+    //    if (Selection.activeGameObject == null) {
+    //        Debug.LogError("No GameObject selected.");
+    //        return;
+    //    }
+    //
+    //    var parentOfObjectsToCombine = Selection.activeGameObject;
+    //
+    //    Vector3 originalPosition = parentOfObjectsToCombine.transform.position;
+    //    parentOfObjectsToCombine.transform.position = Vector3.zero;
+    //
+    //    MeshFilter[] meshFilters = Selection.activeGameObject.GetComponentsInChildren<MeshFilter>(false);
+    //    Dictionary<Material, List<MeshFilter>> materialToMeshFilterList = new Dictionary<Material, List<MeshFilter>>();
+    //    List<GameObject> combinedObjects = new List<GameObject>();
+    //
+    //    for (int i = 0; i < meshFilters.Length; i++) {
+    //        var materials = meshFilters[i].GetComponent<MeshRenderer>().sharedMaterials;
+    //        if (materials == null) continue;
+    //        if (materials.Length > 1) {
+    //            //parentOfObjectsToCombine.transform.position = originalPosition;
+    //            //Debug.LogError("Multiple materials on meshes. Can't combine");
+    //            //return;
+    //        }
+    //
+    //        Material material = materials[0];
+    //        if (materialToMeshFilterList.ContainsKey(material)) materialToMeshFilterList[material].Add(meshFilters[i]);
+    //        else materialToMeshFilterList.Add(material, new List<MeshFilter>() { meshFilters[i] });
+    //    }
+    //
+    //    foreach (var entry in materialToMeshFilterList) {
+    //        List<MeshFilter> meshesWithSameMaterial = entry.Value;
+    //        string materialName = entry.Key.ToString().Split(' ')[0];
+    //        CombineInstance[] combine = new CombineInstance[meshesWithSameMaterial.Count];
+    //        for (int i = 0; i < meshesWithSameMaterial.Count; i++) {
+    //            combine[i].mesh = meshesWithSameMaterial[i].sharedMesh;
+    //            combine[i].transform = meshesWithSameMaterial[i].transform.localToWorldMatrix;
+    //        }
+    //
+    //        Mesh combinedMesh = new Mesh();
+    //        combinedMesh.CombineMeshes(combine, true, true, true);
+    //        materialName += "_" + combinedMesh.GetInstanceID();
+    //        Unwrapping.GenerateSecondaryUVSet(combinedMesh);
+    //        if (createPrefab) AssetDatabase.CreateAsset(combinedMesh, "Assets/ContentPacks/CombinedMeshes/" + materialName + "Combined.asset");
+    //
+    //        string goName = (materialToMeshFilterList.Count > 1) ? materialName + "Combined" : parentOfObjectsToCombine.name + "Combined";
+    //        GameObject combinedObject = new GameObject(goName);
+    //        var filter = combinedObject.AddComponent<MeshFilter>();
+    //        filter.sharedMesh = combinedMesh;
+    //        var renderer = combinedObject.AddComponent<MeshRenderer>();
+    //        renderer.sharedMaterial = entry.Key;
+    //        combinedObjects.Add(combinedObject);
+    //
+    //
+    //    }
+    //
+    //    GameObject resultGO = null;
+    //    if (combinedObjects.Count > 1) {
+    //        resultGO = new GameObject(parentOfObjectsToCombine.name + "Combined");
+    //        foreach (var combinedObject in combinedObjects) combinedObject.transform.parent = resultGO.transform;
+    //    } else {
+    //        resultGO = combinedObjects[0];
+    //    }
+    //
+    //    resultGO.isStatic = true;
+    //    if (parentOfObjectsToCombine.transform.parent != null) {
+    //        resultGO.transform.parent = parentOfObjectsToCombine.transform.parent;
+    //    }
+    //
+    //    if (createPrefab) PrefabUtility.SaveAsPrefabAssetAndConnect(resultGO, "Assets/ContentPacks/CombinedMeshes" + resultGO.name + ".prefab", InteractionMode.UserAction);
+    //    parentOfObjectsToCombine.SetActive(false);
+    //    parentOfObjectsToCombine.transform.position = originalPosition;
+    //    resultGO.transform.position = originalPosition;
+    //}
 }
