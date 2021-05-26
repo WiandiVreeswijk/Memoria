2f001e05122ef7ab4754ff77b08b75fad3590c5e
DESKTOP-9M2UNS6\Manfred Mosterdfiets <wiavre@gmail.com>
2021-05-26 22:44:19 +0200

    Created chasinglevel scene; created new test environment.

--- Memoria/Assets/Scripts/Player/PlayerMovement2D.cs
+++ Memoria/Assets/Scripts/Player/PlayerMovement2D.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerMovement2D : MonoBehaviour
+{
     public float moveSpeed;
     public float jumpForce;
     private float moveInput;
@@ -23,62 +24,79 @@ public class PlayerController : MonoBehaviour {
     public float jumpBufferLength;
     private float jumpBufferCount;
 
-    void Start() {
+    void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
         playerCollider = GetComponent<Collider2D>();
     }
 
-    void Update() {
+    void Update()
+    {
         Jump();
         moveInput = Input.GetAxis("Horizontal");
     }
 
-    private void FixedUpdate() {
+    private void FixedUpdate()
+    {
         isGrounded = Physics2D.IsTouchingLayers(playerCollider, platformLayerMask);
         MoveHorizontal();
     }
 
-    private void MoveHorizontal() {
+    private void MoveHorizontal()
+    {
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
 
-        if (Input.GetAxis("Horizontal") < 0) {
+        if (Input.GetAxis("Horizontal") < 0)
+        {
             transform.rotation = Quaternion.Euler(0, 180, 0);
         }
 
-        if (Input.GetAxis("Horizontal") > 0) {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
             transform.rotation = Quaternion.Euler(0, 0, 0);
         }
     }
-    private void Jump() {
+    private void Jump()
+    {
         //to manage hang time
-        if (isGrounded) {
+        if (isGrounded)
+        {
             hangCounter = hangTime;
-        } else {
+        }
+        else
+        {
             hangCounter -= Time.deltaTime;
         }
 
         //manage jump buffer
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
             jumpBufferCount = jumpBufferLength;
-        } else {
+        }
+        else
+        {
             jumpBufferCount -= Time.deltaTime;
         }
 
         //checks hangtime and jumpbuffer
-        if (jumpBufferCount >= 0 && hangCounter > 0f) {
+        if (jumpBufferCount >= 0 && hangCounter > 0f)
+        {
             rb.velocity = Vector2.up * jumpForce;
             jumpBufferCount = 0;
         }
 
         //checks if player is falling down
-        if (rb.velocity.y < 0) {
+        if (rb.velocity.y < 0)
+        {
             rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
         }
         //checks if player is going up and jump button is released
-        else if (rb.velocity.y > 0 && !Input.GetKeyDown(KeyCode.Space)) {
+        else if (rb.velocity.y > 0 && !Input.GetKeyDown(KeyCode.Space))
+        {
             rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
         }
-        if (rb.velocity.y > 0 && Input.GetKeyUp(KeyCode.Space)) {
+        if (rb.velocity.y > 0 && Input.GetKeyUp(KeyCode.Space))
+        {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
     }
