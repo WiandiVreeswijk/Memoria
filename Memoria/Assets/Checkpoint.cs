08f029b469e17e335e433c0442c8cdc33ee5dc16
DESKTOP-9M2UNS6\Manfred Mosterdfiets <wiavre@gmail.com>
2021-05-30 21:58:28 +0200

    editted water shader.

--- Memoria/Assets/Checkpoint.cs
+++ Memoria/Assets/Checkpoint.cs
@@ -1,36 +1,50 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
-    public Renderer checkpoint;
+    public Renderer checkpointMaterial;
     public Color activateColor, inactiveColor;
+    private bool isActivated = false;
 
     private PlayerRespawn respawn;
+
+    public ParticleSystem particles;
+    public GameObject checkpoint;
+
+    private Tween tween;
+    public Vector3 punch;
+    public float duration = 0.5f, elasticity = 0.3f;
+    public int vibrato = 1;
 
     private void Start()
     {
         respawn = FindObjectOfType<PlayerRespawn>();
+        particles.Stop();
         NotActivated();
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && !isActivated)
         {
             Activated();
+            isActivated = true;
         }
     }
 
     private void Activated()
     {
-        checkpoint.material.color = activateColor;
+        checkpointMaterial.material.color = activateColor;
         respawn.playerStartPosition = transform.position;
+        tween = checkpoint.transform.DOPunchScale(punch, duration, vibrato, elasticity);
+        particles.Play();
     }
 
     private void NotActivated()
     {
-        checkpoint.material.color = inactiveColor;
+        checkpointMaterial.material.color = inactiveColor;
     }
 }
 
