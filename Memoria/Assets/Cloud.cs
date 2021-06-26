1ec48e354676bc5662961968a3338eeebe351486
Erwin van Crasbeek <evancrasbeek@xs4all.nl>
2021-06-26 16:07:07 +0200

    Added prewarm to cloud spawner; Added light blocker to prevent light leakage on background mountains; Added lighting to new start area; Improved background mountains visuals; Improved oblivion edge visuals in the distance.

--- Memoria/Assets/Cloud.cs
+++ Memoria/Assets/Cloud.cs
@@ -2,30 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cloud : MonoBehaviour
-{
+public class Cloud : MonoBehaviour {
     private float speed = 0;
     private float maxDistance = -1;
     private float startX;
 
-    private void Start()
-    {
-        speed = Random.Range(0.1f, 0.11f);
+    private void Start() {
         startX = transform.position.x;
     }
 
-    public void SetMaxDistance(float maxDistance)    {
-        this.maxDistance = maxDistance;
-    }
-
-    void FixedUpdate()
-    {
-        transform.position += new Vector3(speed, 0.0f, 0.0f);
+    void FixedUpdate() {
+        transform.position += new Vector3(speed * Time.fixedDeltaTime, 0.0f, 0.0f);
         if (transform.position.x - startX > maxDistance) Destroy(gameObject);
-        if (maxDistance == -1f)
-        {
+        if (maxDistance == -1f) {
             Destroy(gameObject);
             throw new System.MissingMemberException("Max distance has not been set for cloud!");
         }
+    }
+
+    public void Setup(float maxTravelDistance, float cloudSpeed) {
+        maxDistance = maxTravelDistance;
+        speed = cloudSpeed;
     }
 }
--- Memoria/Assets/CloudSpawner.cs
+++ Memoria/Assets/CloudSpawner.cs
@@ -2,24 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CloudSpawner : MonoBehaviour
-{
+public class CloudSpawner : MonoBehaviour {
     public GameObject[] clouds;
-    public float maxCloudTravelDistance = 10;
+    public Transform cloudRemovePoint;
+    public float cloudSpeed = 0.1f;
+    public float timeBetweenClouds = 2.0f;
+    private float maxCloudTravelDistance;
     private Utils.Cooldown cooldown;
 
-    void FixedUpdate()
-    {
-        if (clouds.Length > 0)
-        {
-            if (cooldown.Ready(10.0f))
-            {
-                GameObject randomPrefab = clouds[Random.Range(0, clouds.Length)];
-                GameObject cloud = Instantiate(randomPrefab, transform.position + new Vector3(0.0f, Random.Range(-20.0f, 20.0f), Random.Range(-40.0f, 40.0f)), Quaternion.identity);
-                cloud.transform.localScale = new Vector3(10f, 10f, 10f);
-                Cloud cloudComponent = cloud.AddComponent<Cloud>();
-                cloudComponent.SetMaxDistance(maxCloudTravelDistance);
-            }
+    void Start() {
+        maxCloudTravelDistance = cloudRemovePoint.position.x - transform.position.x;
+        if (clouds.Length == 0) {
+            gameObject.SetActive(false);
+            throw new System.Exception("No clouds assigned to cloud spawner!");
+        } else Prewarm();
+    }
+    void FixedUpdate() {
+        if (cooldown.Ready(2.0f)) {
+            SpawnCloud(maxCloudTravelDistance);
+        }
+    }
+
+    void SpawnCloud(float maxTravelDistance, float distanceFromSpawn = 0.0f) {
+        GameObject randomPrefab = clouds[Random.Range(0, clouds.Length)];
+        float y = Random.Range(-20.0f, 20.0f);
+        float z = Random.Range(-40.0f, 40.0f);
+        GameObject cloud = Instantiate(randomPrefab, transform.position + new Vector3(distanceFromSpawn, y, z), Quaternion.identity);
+        cloud.transform.localScale = new Vector3(5f, 5f, 5f);
+        Cloud cloudComponent = cloud.AddComponent<Cloud>();
+        cloudComponent.Setup(maxTravelDistance - distanceFromSpawn, cloudSpeed);
+    }
+
+    void Prewarm() {
+        float distance = 0;
+        while (distance < maxCloudTravelDistance) {
+            SpawnCloud(maxCloudTravelDistance, distance);
+            distance += timeBetweenClouds * cloudSpeed;
         }
     }
 }
