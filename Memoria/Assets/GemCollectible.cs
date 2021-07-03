6933f53ea8396673224423a6e65c34c472327e78
Erwin van Crasbeek <evancrasbeek@xs4all.nl>
2021-07-03 14:18:32 +0200

    Added collectible gems; Added collectible gem sound effect.

---
+++ Memoria/Assets/GemCollectible.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class GemCollectible : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    void Update() {
+        //transform.localRotation += Quaternion.Euler(new Vector3(0.0f, 45.0f, 0.0f) * Time.deltaTime);
+    }
+}
