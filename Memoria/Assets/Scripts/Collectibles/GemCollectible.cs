4c38911bfb481820a8b66ac6c8c88d43b0a1731d
Erwin van Crasbeek <evancrasbeek@xs4all.nl>
2021-07-10 01:17:56 +0200

    Fixed gem collectible sound not playing; Improved fireplace light intensity.

--- Memoria/Assets/Scripts/Collectibles/GemCollectible.cs
+++ Memoria/Assets/Scripts/Collectibles/GemCollectible.cs
@@ -29,7 +29,7 @@ public class GemCollectible : MonoBehaviour, IEnterActivatable {
         GameObject instantiated = Instantiate(visualPrefab);
         instantiated.transform.position = new Vector3(Globals.Player.HeadPosition.x, Globals.Player.HeadPosition.y + 0.5f, Globals.Player.HeadPosition.z);
         instantiated.transform.parent = Globals.UIManager.ScreenspaceCanvas.transform;
-        FMODUnity.RuntimeManager.PlayOneShot(soundEffect, instantiated.transform.position);
+        FMODUnity.RuntimeManager.PlayOneShot(soundEffect);
     }
 
     public void PlayPFX() {
