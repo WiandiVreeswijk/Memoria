ff575b21bcf77d3848547fd5ddca385ad5c54c36
DESKTOP-9M2UNS6\Manfred Mosterdfiets <wiavre@gmail.com>
2021-07-10 19:36:27 +0200

    added custom events for tracking player progression (Unity analytics).

--- Memoria/Assets/AnalyticsManager.cs
+++ Memoria/Assets/AnalyticsManager.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Analytics;
 
 public class AnalyticsManager : MonoBehaviour {
     public void OnPlayerDeath(Vector3 position, bool water) {
-
+        Analytics.CustomEvent("Player Dies", new Dictionary<string, object> {
+                {"Position", position},
+                {"is Water", water}
+            }); ;
     }
     public void OnCheckpointReached(int checkpointIndex) {
-
+        Analytics.CustomEvent("Checkpoint reached", new Dictionary<string, object> {
+                {"checkpoint reached", checkpointIndex},
+            }); ;
     }
 
     public void OnLevelStarted() {
@@ -15,6 +21,9 @@ public class AnalyticsManager : MonoBehaviour {
     }
 
     public void OnLevelEnded() {
-        Globals.ScoreManager.GetCollectibleCount();
+        Analytics.CustomEvent("Level completed", new Dictionary<string, object> {
+                {"Gems collected", Globals.ScoreManager.GetCollectibleCount()},
+                {"Play duration level", Time.timeSinceLevelLoad}
+            }); ;
     }
 }
