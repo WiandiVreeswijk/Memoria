c4028fe549a171b5f347e119a353ce9e51a3e93f
DESKTOP-9M2UNS6\Manfred Mosterdfiets <wiavre@gmail.com>
2021-07-13 18:10:14 +0200

    improved custom events meanings; converted time stamps to minutes; analytics cleared up.

--- Memoria/Assets/AnalyticsManager.cs
+++ Memoria/Assets/AnalyticsManager.cs
@@ -6,13 +6,13 @@ using UnityEngine.Analytics;
 public class AnalyticsManager : MonoBehaviour {
     public void OnPlayerDeath(Vector3 position, bool water) {
         Analytics.CustomEvent("Player Dies", new Dictionary<string, object> {
-                {"Position", position},
+                {"Position", position.x},
                 {"is Water", water}
             }); ;
     }
     public void OnCheckpointReached(int checkpointIndex) {
         Analytics.CustomEvent("Checkpoint reached", new Dictionary<string, object> {
-                {"checkpoint reached", checkpointIndex},
+                {"checkpoint index", checkpointIndex + " Time (minutes)" + Time.timeSinceLevelLoad /60}
             }); ;
     }
 
@@ -23,7 +23,7 @@ public class AnalyticsManager : MonoBehaviour {
     public void OnLevelEnded() {
         Analytics.CustomEvent("Level completed", new Dictionary<string, object> {
                 {"Gems collected", Globals.ScoreManager.GetCollectibleCount()},
-                {"Play duration level", Time.timeSinceLevelLoad}
+                {"Play duration (minutes)", Time.timeSinceLevelLoad /60}
             }); ;
     }
 }
