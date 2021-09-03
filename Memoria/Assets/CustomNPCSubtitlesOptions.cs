521f358ac7590f085ac6804d06b76dbe826f2c04
DESKTOP-9M2UNS6\Manfred Mosterdfiets <wiavre@gmail.com>
2021-09-03 13:26:01 +0200

    First version of dialogue system finished; created expertiment conversation in dialogue test scene.

---
+++ Memoria/Assets/CustomNPCSubtitlesOptions.cs
@@ -0,0 +1,13 @@
+using PixelCrushers.DialogueSystem;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomNPCSubtitlesOptions : MonoBehaviour
+{
+    public StandardUISubtitlePanel panel;
+
+    private void Start()
+    {
+    }
+}
