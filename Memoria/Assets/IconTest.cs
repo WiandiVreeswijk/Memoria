abb7f2529e53298d12fc1ad793d7d8f96d7cfe88
Erwin <evancrasbeek@xs4all.nl>
2021-09-06 16:21:12 +0200

    Improved icon system.

--- Memoria/Assets/IconTest.cs
+++ Memoria/Assets/IconTest.cs
@@ -5,15 +5,10 @@ using UnityEngine;
 public class IconTest : MonoBehaviour {
     // Start is called before the first frame update
     private RectTransform rt;
-    void Start()
-    {
+    void Start() {
         rt = GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
-    void Update() {
-        Vector2 sp = Globals.Camera.WorldToScreenPoint(new Vector3(15, 3, 18));
-        print(sp);
-        rt.position= new Vector3(sp.x, sp.y, 0);
-    }
+
 }
--- Memoria/Assets/Scripts/Icon.cs
+++ Memoria/Assets/Scripts/Icon.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Icon : MonoBehaviour {
     public GameObject iconObject;
     public GameObject iconZObject;
+    private RectTransform rt;
 
     public void SetImage(Texture texture) {
         Material material = iconObject.GetComponent<MeshRenderer>().material;
@@ -13,9 +14,15 @@ public class Icon : MonoBehaviour {
         materialZ.SetTexture("_BaseMap", texture);
     }
 
-    public void Update()
-    {
-        Vector2 point = Globals.Camera.WorldToScreenPoint(transform.position);
-        transform.position = point;
+    void Start() {
+        rt = GetComponent<RectTransform>();
+    }
+    void LateUpdate() {
+        Globals.Camera.ResetWorldToCameraMatrix();
+        var sp = Globals.Camera.WorldToScreenPoint(new Vector3(15, 3, 18));
+        if (sp.z > 0) {
+            sp.z = 0;
+            rt.SetPositionAndRotation(new Vector3(sp.x, sp.y, 0), Quaternion.identity);
+        } else rt.position = new Vector2(-1000, -1000);
     }
 }
