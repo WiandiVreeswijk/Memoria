871239777dbad36eceb88a58aeea1900e7fa11c9
Erwin <evancrasbeek@xs4all.nl>
2021-09-09 16:26:21 +0200

    Fixed bus audio fade.

--- Memoria/Assets/Scripts/Audio/SoundManagerWijk.cs
+++ Memoria/Assets/Scripts/Audio/SoundManagerWijk.cs
@@ -3,36 +3,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoundManagerWijk : MonoBehaviour
-{
-    BusEngineSound busEngineSound;
-
+public class SoundManagerWijk : MonoBehaviour {
+    public BusEngineSound busEngineSound;
     private Tween engineStatusFadeTween;
     private Tween volumeFadeTween;
-    public void FadeEngineStatus(float engineValue, float duration = 2.5f)
-    {
+    public void FadeEngineStatus(float engineValue, float duration = 2.5f) {
         engineStatusFadeTween?.Kill();
-        if (duration == 0.0f)
-        {
+        if (duration == 0.0f) {
             busEngineSound.SetEngineStatus(engineValue);
-        }
-        else
-        {
+        } else {
             engineStatusFadeTween = DOTween
                 .To(() => busEngineSound.GetEngineStatus(), x => busEngineSound.SetEngineStatus(x), engineValue, 1f)
-                .SetEase(Ease.Linear).OnUpdate(()=>print(1));
+                .SetEase(Ease.Linear);
         }
     }
 
-    public void FadeEngineVolume(float volumeValue, float duration = 2.5f)
-    {
+    public void FadeEngineVolume(float volumeValue, float duration = 2.5f) {
         volumeFadeTween?.Kill();
-        if (duration == 0.0f)
-        {
+        if (duration == 0.0f) {
             busEngineSound.SetVolume(volumeValue);
-        }
-        else
-        {
+        } else {
             volumeFadeTween = DOTween
                 .To(() => busEngineSound.GetVolume(), x => busEngineSound.SetVolume(x), volumeValue, 1f)
                 .SetEase(Ease.Linear);
