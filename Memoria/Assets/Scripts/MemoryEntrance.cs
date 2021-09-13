05cf4f8396375eec628557c39bbfc230c171ede2
Erwin <evancrasbeek@xs4all.nl>
2021-09-13 11:14:20 +0200

    Added more UI elements to memory entry portal; Added main 2D shader.

--- Memoria/Assets/Scripts/MemoryEntrance.cs
+++ Memoria/Assets/Scripts/MemoryEntrance.cs
@@ -6,31 +6,57 @@ using DG.Tweening;
 namespace Assets.Scripts {
     public class MemoryEntrance : MonoBehaviour {
         public string sceneName;
-        public CanvasGroup group;
-        public List<Component> uiComponents;
+        public GameObject group;
+        public GameObject portal;
+        private List<Transform> uiComponents;
         private bool open = true;
+
+        private List<Tween> tweens = new List<Tween>();
+
+        public void Start() {
+            for (int i = 0; i < group.transform.childCount; i++) {
+                uiComponents.Add(group.transform.GetChild(i));
+            }
+        }
+
         public void OnActivateWatch() {
             open ^= true;
             if (open) Open();
             else Close();
         }
 
+        private void KillTweens() {
+            foreach (var tween in tweens) tween.Kill();
+            tweens.Clear();
+        }
+
         private void Open() {
-            group.transform.DOScale(Vector3.one, 1.0f).SetEase(Ease.InElastic);
+            KillTweens();
+            tweens.Add(group.transform.DOScale(Vector3.one, 1.0f).SetEase(Ease.InElastic));
             uiComponents.ForEach(x => {
-                x.transform.DOScale(Vector3.one, 1.0f);
+                tweens.Add(x.transform.DOScale(Vector3.one, 1.0f));
             });
         }
 
         private void Close() {
-            group.transform.DOScale(Vector3.zero, 1.0f).SetEase(Ease.InElastic);
+            KillTweens();
+            tweens.Add(group.transform.DOScale(Vector3.zero, 1.0f).SetEase(Ease.InElastic));
             uiComponents.ForEach(x => {
-                x.transform.DOScale(Vector3.one, 0.0f);
+                tweens.Add(x.transform.DOScale(Vector3.one, 0.0f));
             });
         }
 
         public void Enter() {
             Globals.SceneManager.SetScene(sceneName);
+        }
+
+        public void FixedUpdate() {
+            float distance = Vector3.Distance(Globals.Player.transform.position, transform.position);
+            if (distance < 5 && !open) {
+                Open();
+            } else if (distance > 5 && open) {
+                Close();
+            }
         }
     }
 }
