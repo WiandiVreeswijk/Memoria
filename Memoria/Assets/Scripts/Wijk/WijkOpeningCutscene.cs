b77b099fa43c84e64270955b0269da2bfe810221
Erwin <evancrasbeek@xs4all.nl>
2021-09-22 11:13:17 +0200

    Improved introduction notifications.

--- Memoria/Assets/Scripts/Wijk/WijkOpeningCutscene.cs
+++ Memoria/Assets/Scripts/Wijk/WijkOpeningCutscene.cs
@@ -21,7 +21,7 @@ public class WijkOpeningCutscene : MonoBehaviour {
 
     public void OnFinishCutscene() {
         Globals.Player.PlayerMovementAdventure.SetCanMove(true);
-        Utils.DelayedAction(10.0f, () => {
+        Utils.DelayedAction(7.5f, () => {
             if (!Globals.Player.PlayerMovementAdventure.HasMoved()) Globals.MenuController.NotifyPlayer("WASD to move");
         });
 
@@ -30,11 +30,11 @@ public class WijkOpeningCutscene : MonoBehaviour {
 
     private void StartMarkerNotification() {
         Tween tween = null;
-        tween = Utils.DelayedAction(64.0f, () => {
+        tween = Utils.DelayedAction(20.0f, () => {
             Globals.MenuController.NotifyPlayer("Follow the ! marker");
         }).OnUpdate(() => {
             if (Vector3.Distance(Globals.Player.transform.position, Globals.ProgressionManager.GetIcon().transform.position) <
-                2.5f) {
+                3.5f) {
                 print("Player got close to marker!");
                 tween.Kill();
             }
@@ -46,7 +46,7 @@ public class WijkOpeningCutscene : MonoBehaviour {
     }
 
     private void MouseNotification() {
-        Globals.MenuController.NotifyPlayer("<font=\"Mouse SDF\"><size=36>u</size></font>  to move the camera");
+        Globals.MenuController.NotifyPlayer("<font=\"Mouse SDF\"><size=78>u</size></font>  to move the camera");
     }
 
     public void Trigger() {
@@ -66,7 +66,7 @@ public class WijkOpeningCutscene : MonoBehaviour {
             MouseNotification();
             OnFinishCutscene();
         } else {
-            Utils.DelayedAction(10.0f, () => MouseNotification());
+            Utils.DelayedAction(15.0f, () => MouseNotification());
         }
     }
 }
