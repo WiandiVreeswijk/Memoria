adafa4e410d647ee7080e93299069396d252dd4c
Wiandi Vreeswijk <wiavre@gmail.com>
2021-09-29 15:49:15 +0200

    Added screen mode functionality to options menu; added resolutions options functionality to options menu; revamped options menu UI; added volume slider in options menu.

--- Memoria/Assets/Scripts/Menu/OptionsMenu.cs
+++ Memoria/Assets/Scripts/Menu/OptionsMenu.cs
@@ -4,7 +4,70 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.UI;
+using TMPro;
 
 public class OptionsMenu : MonoBehaviour {
-   
+
+    private string masterBusString = "Bus:/";
+    private FMOD.Studio.Bus masterBus;
+
+    Resolution[] resolutions;
+    public TMP_Dropdown resolutionDropdown;
+    public TMP_Dropdown graphicsDropdown;
+
+    private void Start()
+    {
+        masterBus = FMODUnity.RuntimeManager.GetBus(masterBusString);
+
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+
+        graphicsDropdown.value = 5;
+
+
+    }
+
+    public void SetVolume(float volume)
+    {
+        masterBus.setVolume(volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(int screenTypeIndex)
+    {
+        if(screenTypeIndex == 0)
+        {
+            Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+        }else if(screenTypeIndex == 1)
+        {
+            Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+        }else if(screenTypeIndex == 2)
+        {
+            Screen.fullScreenMode = FullScreenMode.Windowed;
+        }
+    }
 }
