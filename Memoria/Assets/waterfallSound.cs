4101a83d717b8441061c2bb06158e7b234feb1e0
DESKTOP-9M2UNS6\Manfred Mosterdfiets <wiavre@gmail.com>
2021-09-30 12:44:09 +0200

    added sound for waterfalls

---
+++ Memoria/Assets/waterfallSound.cs
@@ -0,0 +1,38 @@
+using FMOD.Studio;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waterfallSound : MonoBehaviour
+{
+    //FMOD
+    [Header("FMOD Event")]
+    [FMODUnity.EventRef]
+    public string SelectAudio;
+    public new FMOD.Studio.EventInstance audio;
+    private PARAMETER_ID volumeParameter;
+
+    [Header("waterfall Volume")]
+    [Range(0f, 1f)]
+    public float volumeValue = 0f;
+    public float volumeMultiplier;
+
+    private void Start()
+    {
+        audio = FMODUnity.RuntimeManager.CreateInstance(SelectAudio);
+
+        FMOD.Studio.EventDescription volumeDescription;
+        audio.getDescription(out volumeDescription);
+        FMOD.Studio.PARAMETER_DESCRIPTION volumeParameterDescription;
+        volumeDescription.getParameterDescriptionByName("waterfallVolume", out volumeParameterDescription);
+        volumeParameter = volumeParameterDescription.id;
+    }
+
+    private void FixedUpdate()
+    {
+        float distance = Utils.Distance(Globals.Player.transform.position.x, transform.position.x);
+        volumeValue = Mathf.Lerp(1.0f, 0, distance * volumeMultiplier);
+        audio.setParameterByID(volumeParameter, volumeValue);
+    }
+
+}
