1a3bc0c1034a786c8fd325b34df13a10d2b9f080
DESKTOP-9M2UNS6\Manfred Mosterdfiets <wiavre@gmail.com>
2021-10-05 20:56:39 +0200

    muted waterfall sound when transitioning back to wijk scene.

--- Memoria/Assets/Scripts/Other/ChangeSceneActivatable.cs
+++ Memoria/Assets/Scripts/Other/ChangeSceneActivatable.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class ChangeSceneActivatable : MonoBehaviour, IEnterActivatable {
     public string sceneName;
 
+    private waterfallSound waterfallSound;
+
+    private void Start()
+    {
+        waterfallSound = GetComponent<waterfallSound>();
+    }
+
     public void ActivateEnter() {
         Globals.SoundManagerChase.FadeMusicVolume(0.0f);
         Globals.SoundManagerChase.FadeAmbientVolume(0.0f);
+        waterfallSound.MuteWaterfall();
         Globals.Player.PlayerMovement25D.SetStunned(true, true, true);
         Globals.SceneManager.SetScene(sceneName);
     }
