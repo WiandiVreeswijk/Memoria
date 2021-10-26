8c8daedf92bc84af4078495c7adcf5971d875b56
Erwin <evancrasbeek@xs4all.nl>
2021-10-26 16:51:32 +0200

    Added Steam keys; Added some missing steam logos.

--- Memoria/Assets/SteamworksManager.cs
+++ Memoria/Assets/SteamworksManager.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class SteamworksManager : MonoBehaviour {
     void Awake() {
         try {
             Steamworks.SteamClient.Init(1791770);
-        } catch (System.Exception e)
-        {
-            throw e;
+        } catch (System.Exception e) {
+            print(e.Message);
+            //throw e;
             // Something went wrong - it's one of these:
             //
             //     Steam is closed?
@@ -19,6 +20,10 @@ public class SteamworksManager : MonoBehaviour {
     }
 
     void Update() {
+        //if (Input.GetKeyDown(KeyCode.Space)) Steamworks.SteamUserStats.IndicateAchievementProgress("Fiets", 1, 100);
+        foreach (var c in Steamworks.SteamInput.Controllers) {
+            print(c.Id);
+        }
         Steamworks.SteamClient.RunCallbacks();
     }
 
